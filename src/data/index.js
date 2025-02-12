5e7a98cc83577477a6851ef62eb2ac4bcfd6a200
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-11 23:22:08 -0300

    feat: created file to provide datas

---
+++ src/data/index.js
@@ -0,0 +1,16 @@
+import {
+  FaUserAlt,
+  FaFolderOpen,
+  FaGraduationCap,
+  FaHome,
+  FaGithub
+} from "react-icons/fa";
+
+// Navigation Links Data
+export const liValues = [
+  { id: 0, text: "Home", icon: FaHome, idLink: "home" },
+  { id: 1, text: "Quem sou", icon: FaUserAlt, idLink: "quem-somos" },
+  { id: 2, text: "Projetos", icon: FaFolderOpen, idLink: "projetos" },
+  { id: 3, text: "Formação", icon: FaGraduationCap, idLink: "midia" },
+  { id: 4, text: "Github", icon: FaGithub, idLink: "contato" },
+];
