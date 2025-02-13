386b565f4de401025f73ba9b86fbafea4ef0b0d0
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-13 19:13:48 -0300

    feat: created component Info Security

---
+++ src/components/Infos/InfoSecurity/index.jsx
@@ -0,0 +1,18 @@
+import * as S from "./styles"
+
+export const InfoSecurity = () => {
+  return (
+    <S.InfoSecurity>
+      <S.InfoCards>
+        <S.ColumnMobile>
+          coluna 1
+        </S.ColumnMobile>
+
+        <S.ColumnSecurity>
+          coluna 2
+        </S.ColumnSecurity>
+      </S.InfoCards>
+      <S.ImageCards>Image</S.ImageCards>
+    </S.InfoSecurity>
+  )
+}
---
+++ src/components/Infos/InfoSecurity/styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const InfoSecurity = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+
+  @media (max-width: 900px) {
+    flex-direction: column-reverse;
+    height: 100%;
+  }
+`;
+
+export const InfoCards = styled.div`
+  width: 50%;
+  height: 100%;
+
+  position: relative;
+
+  display: flex;
+  justify-content: space-around;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    height: 50%;
+  }
+`;
+
+export const ColumnMobile = styled.div`
+  position: absolute;
+
+  left: 5rem;
+  bottom: 0;
+
+  width: 280px;
+  height: 320px;
+  background: var(--color-12);
+`;
+
+export const ColumnSecurity = styled.div`
+  position: absolute;
+
+  right: 5rem;
+  bottom: 0;
+
+  width: 280px;
+  height: 320px;
+  background: var(--color-12);
+`;
+
+export const ImageCards = styled.div`
+  flex: 1;
+  width: 50%;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    height: 50%;
+  }
+
+  background: pink;
+`;
