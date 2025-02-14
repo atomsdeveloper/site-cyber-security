d5a1073ce54977052898a92b51c899c9281af614
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-13 21:55:59 -0300

    fix: refactor component to styled components

--- src/components/Content/InfoSecurity/index.jsx
+++ src/components/Content/InfoSecurity/index.jsx
@@ -1,15 +1,44 @@
 import * as S from "./styles"
 
+// Components
+import { Text } from "./Text";
+
+// Icons
+import { RiMobileDownloadFill } from "react-icons/ri";
+import { MdSecurity } from "react-icons/md";
+
 export const InfoSecurity = () => {
   return (
     <S.InfoSecurity>
       <S.InfoCards>
         <S.ColumnMobile>
-          coluna 1
+          <S.Container>
+            <S.CircleWhite>
+              <RiMobileDownloadFill
+                size={100}
+              />
+            </S.CircleWhite>
+          </S.Container>
+
+          <Text
+            title={"Mobile Acess"}
+            text={"Lorem ipsum text about title and more notifications"}
+          />
         </S.ColumnMobile>
 
         <S.ColumnSecurity>
-          coluna 2
+          <S.Container>
+            <S.CircleWhite>
+              <MdSecurity
+                size={100}
+              />
+            </S.CircleWhite>
+          </S.Container>
+
+          <Text
+            title={"Coding and Security"}
+            text={"Lorem ipsum text about title and more notifications"}
+          />
         </S.ColumnSecurity>
       </S.InfoCards>
       <S.ImageCards>Image</S.ImageCards>
--- src/components/Content/InfoSecurity/styles.js
+++ src/components/Content/InfoSecurity/styles.js
@@ -5,7 +5,7 @@ export const InfoSecurity = styled.div`
   height: 50%;
   display: flex;
 
-  @media (max-width: 900px) {
+  @media (max-width: 1000px) {
     flex-direction: column-reverse;
     height: 100%;
   }
@@ -20,32 +20,110 @@ export const InfoCards = styled.div`
   display: flex;
   justify-content: space-around;
 
-  @media (max-width: 900px) {
+  @media (max-width: 1000px) {
     width: 100%;
     height: 50%;
   }
 `;
 
 export const ColumnMobile = styled.div`
+  width: 180px;
+  height: 95%;
+
   position: absolute;
 
-  left: 5rem;
+  left: 4rem;
   bottom: 0;
 
-  width: 280px;
-  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  border-radius: 50rem 50rem 0 0;
   background: var(--color-12);
+
+  @media (max-width: 1000px) {
+    left: 8rem;
+  }
+
+  @media (max-width: 650px) {
+    left: 5rem;
+  }
+
+  @media (max-width: 515px) {
+    left: 2rem;
+  }
+
+  @media (max-width: 430px) {
+    width: 145px;
+  }
+`;
+
+export const Container = styled.div`
+  width: 180px;
+  height: 180px;
+
+  position: relative;
+  top: -18;
+
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 430px) {
+    width: 145px;
+  }
+`;
+
+export const CircleWhite = styled.div`
+  width: 160px;
+  height: 160px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 0.7rem;
+
+  border-radius: 50%;
+  background: var(--color-0);
+
+  @media (max-width: 430px) {
+    width: 130px;
+    height: 130px;
+  }
 `;
 
 export const ColumnSecurity = styled.div`
+  width: 180px;
+  height: 95%;
+
   position: absolute;
 
-  right: 5rem;
+  right: 4rem;
   bottom: 0;
 
-  width: 280px;
-  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  border-radius: 50rem 50rem 0 0;
   background: var(--color-12);
+
+  @media (max-width: 1000px) {
+    right: 8rem;
+  }
+
+  @media (max-width: 640px) {
+    right: 5rem;
+  }
+
+  @media (max-width: 515px) {
+    right: 2rem;
+  }
+
+  @media (max-width: 430px) {
+    width: 145px;
+  }
 `;
 
 export const ImageCards = styled.div`
